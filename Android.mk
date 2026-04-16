b9979db0ed74fd393b72044293739833cac87298
nhansp <nhan.094.vn@gmail.com>
2026-04-16 09:26:48 +0000

    panther: Import blobs & firmware from BP4A.251205.006

---
+++ Android.mk
@@ -0,0 +1,19 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),panther)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,557520b24976c96629089d965fb19792a240951b)
+$(call add-radio-file-sha1-checked,radio/bl1.img,7b36fe0684cdccd4c5edae6e160e8f3df8ac5bd6)
+$(call add-radio-file-sha1-checked,radio/bl2.img,0eddd59fa89d867192aca4bcc6011950e16c47dd)
+$(call add-radio-file-sha1-checked,radio/bl31.img,281737a5224a610b3bbd4b8849d32e9880e67eff)
+$(call add-radio-file-sha1-checked,radio/gsa.img,fa71742bf373c2b9a51a8942595d20a8ce4f09c7)
+$(call add-radio-file-sha1-checked,radio/ldfw.img,0ccd58a9eee485e073f81c20b3168cdd991a84b4)
+$(call add-radio-file-sha1-checked,radio/modem.img,92207f66f5b1cdbc8530c800d704425349d7a2aa)
+$(call add-radio-file-sha1-checked,radio/pbl.img,2caffc8683396717c3eae7dbf4d220218a92a1ef)
+$(call add-radio-file-sha1-checked,radio/tzsw.img,3d917862b96aebbb709f28b9cd187d0dcfe170cb)
+
+endif
